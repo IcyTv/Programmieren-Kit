a09aeffc7011a45abc056f5dc589cacb22cb8a37
Michael Finger <michael.finger@icytv.de>
2020-12-08 15:35:23 +0100

    add to path

--- install.bat
+++ install.bat
@@ -8,6 +8,7 @@ del %path%\checkstyle.bat>NUL 2>NUL
 echo @echo off >> %path%\checkstyle.bat
 echo python3 %path%\checkstyle.py %%* >> %path%\checkstyle.bat
 copy .\checkstyle.py %path%
+%SystemRoot%\system32\SETX.exe PATH "%PATH%";"%path%";
 
 :NORMALIZEPATH
   SET RETVAL=%~f1
